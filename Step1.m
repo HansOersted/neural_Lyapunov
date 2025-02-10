6a974f896729bcf3e9c8d4a9289e6941458c884d
Zhe Shen <lucidshenzhe@gmail.com>
2025-02-10 21:35:14 +0100

    correct the data collector
    Signed-off-by: Zhe Shen <lucidshenzhe@gmail.com>

--- Step1.m
+++ Step1.m
@@ -10,16 +10,20 @@ simulation_time = sample_time * length;
 n1 = 1;
 dimension = 2;
 
-init_dr = [1; 1];
+
 
 for i = 1 : n1
     initial(i).init_dq = [0.8/i; 0.5/i];
     initial(i).init_q = [0; 0];
+
+    initial(i).init_dr = [1; 1];
 end
 
 for i = 1 : n1
     init_dq = initial(i).init_dq;
     init_q = initial(i).init_q;
+
+    init_dr = initial(i).init_dr;
     
     data_from_simulink = sim('circular_data');
     
@@ -27,17 +31,22 @@ for i = 1 : n1
     dq_values = data_from_simulink.dq.signals.values;
     ddq_values = data_from_simulink.ddq.signals.values;
 
+    dr_values = data_from_simulink.dr.signals.values;
+
     de_values = data_from_simulink.de.signals.values;
-    dde_values = -ddq_values;
+    dde_values = data_from_simulink.dde.signals.values;
     
     q_vec = squeeze(q_values)';
     dq_vec = squeeze(dq_values)';
     ddq_vec = squeeze(ddq_values)';
     
+    dr_vec = dr_values;
+
     de_vec = squeeze(de_values)';
     dde_vec = squeeze(dde_values)';
 
-    derivative_training_sample(i).data = de_vec;
-    derivative_derivative_training_sample(i).data = dde_vec;
+    % derivative_training_sample(i).data = de_vec;
+    % derivative_derivative_training_sample(i).data = dde_vec;
 end
 
+%% Plot the reference velocity and the actual velocity
